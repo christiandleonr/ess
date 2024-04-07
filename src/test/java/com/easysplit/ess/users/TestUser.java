4ce775dd0d89aee69aaf36ca5a4ce70a5d2532ac
Christian Ramirez de Leon <christiandleonr@gmail.com>
2024-04-07 16:30:50 -0700

    Update create user endpoint to ignore the username and the lastname if they are not included in the payload

--- src/test/java/com/easysplit/ess/users/TestUser.java
+++ src/test/java/com/easysplit/ess/users/TestUser.java
@@ -1,10 +1,13 @@
 package com.easysplit.ess.users;
 
+import com.easysplit.ess.shared.asserters.ErrorAsserter;
 import com.easysplit.ess.shared.utils.TestUtils;
 import com.easysplit.ess.user.domain.models.User;
 import com.easysplit.ess.users.asserters.UserAsserter;
 import com.easysplit.ess.users.builders.UserBuilder;
 import com.easysplit.ess.users.utils.TestUserHelper;
+import com.easysplit.shared.domain.exceptions.ErrorKeys;
+import com.easysplit.shared.domain.models.ErrorResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,7 +41,51 @@ public class TestUser {
     }
 
     /**
-     * Validates that we can fetch an existing using
+     * Attempts to create a user with a null lastname, null lastnames are invalid,
+     * and we should get the proper error message
+     */
+    @Test
+    public void testCreateUserNullLastName() {
+        String uniqueString = TestUtils.generateUniqueString();
+
+        UserBuilder userBuilder = new UserBuilder();
+        User expectedUser = userBuilder.setName("Name-" + uniqueString)
+                .setUsername("Username-" + uniqueString)
+                .setEmail(uniqueString + "@gmail.com")
+                .setPhone(TestUtils.generate10DigitNumber() + "")
+                .setPassword("Password")
+                .build();
+
+        User actualUser = testUserHelper.createUser(expectedUser, HttpStatus.CREATED);
+        new UserAsserter(actualUser).assertUser(expectedUser);
+
+        testUserHelper.deleteUser(actualUser.getId());
+    }
+
+    /**
+     * Attempts to create a user with a null username, null usernames are invalid,
+     * and we should get the proper error message
+     */
+    @Test
+    public void testCreateUserNullUsername() {
+        String uniqueString = TestUtils.generateUniqueString();
+
+        UserBuilder userBuilder = new UserBuilder();
+        User expectedUser = userBuilder.setName("Name-" + uniqueString)
+                .setLastname("Lastname-" + uniqueString)
+                .setEmail(uniqueString + "@gmail.com")
+                .setPhone(TestUtils.generate10DigitNumber() + "")
+                .setPassword("Password")
+                .build();
+
+        User actualUser = testUserHelper.createUser(expectedUser, HttpStatus.CREATED);
+        new UserAsserter(actualUser).assertUser(expectedUser);
+
+        testUserHelper.deleteUser(actualUser.getId());
+    }
+
+    /**
+     * Validates that we can fetch an existing user
      */
     @Test
     public void testGetUser() {
